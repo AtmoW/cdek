b5408f96e562262abd11b80a590a69d3db452bae
Ilya Evdokimov <devdokimov.ilya@yandex.ru>
2020-02-06 19:26:22 +0300

    добавлен список пвз

--- cdek.php
+++ cdek.php
@@ -1,6 +1,23 @@
 <?php
-    
+    require_once 'vendor/autoload.php';
 
+    $client = new \CdekSDK\CdekClient('', '');
+    
+    use CdekSDK\Requests;
+
+    $request = new Requests\PvzListRequest();
+    $response = $client->sendPvzListRequest($request);
+
+    foreach ($response as $item) {
+        $item->Code;
+        $item->Name;
+        $item->Address;
+    
+        foreach ($item->OfficeImages as $image) {
+            $image->getUrl();
+        }
+        echo $item->Name;
+    }
 
 
 ?>
