f3948cd68748f554b3844a532f18e150b151954a
Ilya Evdokimov <devdokimov.ilya@yandex.ru>
2020-02-06 19:39:04 +0300

    выборка ПВЗ по городу

--- cdek.php
+++ cdek.php
@@ -5,18 +5,24 @@
     
     use CdekSDK\Requests;
 
-    $request = new Requests\PvzListRequest();
-    $response = $client->sendPvzListRequest($request);
-
-    foreach ($response as $item) {
-        $item->Code;
-        $item->Name;
-        $item->Address;
-    
-        foreach ($item->OfficeImages as $image) {
-            $image->getUrl();
+    function getPointsByCity($city, $client){
+        $request = new Requests\PvzListRequest();
+        $response = $client->sendPvzListRequest($request);
+        $points = [];
+        foreach ($response as $item) {
+            if(mb_strtolower($item->City) == trim(mb_strtolower($city)))
+            {
+                $point = [
+                    'code' => $item->Code,
+                    'name' => $item->Name,
+                    'adress' => $item->Address,
+                    'city' =>  $item->City
+                ];
+                $points[] = $point;
+            }
         }
-        echo $item->Name;
+    
+        return $points;
     }
 
 
--- index.php
+++ index.php
@@ -1,3 +1,6 @@
 <?php
     include 'cdek.php';
+
+    var_dump(getPointsByCity('москва',$client));
+    
 ?>
