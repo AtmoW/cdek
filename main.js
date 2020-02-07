e1f50c6ceaa3457090d0e978d446876f7fd76561
Ilya Evdokimov <devdokimov.ilya@yandex.ru>
2020-02-07 12:59:17 +0300

    нахождение всех ПВЗ в городе

--- main.js
+++ main.js
@@ -1,16 +1,17 @@
 $(document).ready(function () {
     $('#cities').change(function(){
         value = $('#cities option:selected').attr('value');
-
+        $('.points').html('Поиск ПВЗ');
         $.post("api.php", {"city_name":value},
         function(data){
-        data.foreach(function(item){
+            $('.points').html('');
+        data.forEach(function(item){
             $('.points').append('<label for="'+item['code']+'">'+
                 '<div class="point__name">'+item['name']+'</div>'+
                 '<div class="point__adress">'+item['adress']+'</div></label>'+
                 '<input id = "'+item['code']+'" name = "point" type="radio" value = "'+item['code']+'">'
                 );
         });
-        });
+        }, 'json');
     });
 });
